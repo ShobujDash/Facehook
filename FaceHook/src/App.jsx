ff1f65c0057b8394fe198e6f03ea40795ed156c0
ShobujDas <shobujd6.gmail.com>
2024-08-14 03:38:26 +0600

    first commit

---
+++ FaceHook/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import {Routes,Route} from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import ProfilePage from './pages/ProfilePage'
+import RegistrationPage from './pages/RegistrationPage'
+import NotFoundPage from './pages/NotFoundPage'
+
+function App() {
+
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/me" element={<ProfilePage />} />
+      <Route path="/register" element={<RegistrationPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  );
+}
+
+export default App
